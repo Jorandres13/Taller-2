9ee8d15738bbeea5873c7025e34613e5ecbd43fb
Jorandres13 <jaguerrero39@utpl.edu.ec>
2024-10-17 12:23:56 -0500

    primer commit

---
+++ Proyecto1/src/proyecto1/Proyecto1.java
@@ -0,0 +1,7 @@
+package proyecto1;
+public class Proyecto1 {
+    public static void main(String[] args) {
+        System.out.print("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+    }
+    
+}
