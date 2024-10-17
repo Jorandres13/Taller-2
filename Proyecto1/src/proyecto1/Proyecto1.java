bfb1c1fb0854ce07731f0361dc5e7d6dc3d944b4
Jorandres13 <jaguerrero39@utpl.edu.ec>
2024-10-17 12:36:51 -0500

    segundo commit

--- Proyecto1/src/proyecto1/Proyecto1.java
+++ Proyecto1/src/proyecto1/Proyecto1.java
@@ -1,7 +1,10 @@
 package proyecto1;
 public class Proyecto1 {
     public static void main(String[] args) {
-        System.out.print("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+        System.out.print("Ejemplo de Java\n" +
+"Manejado desde GitHub\n" +
+"Versionado a través de Git\n" +
+"Creado por <Jorandres13>");
     }
     
 }
